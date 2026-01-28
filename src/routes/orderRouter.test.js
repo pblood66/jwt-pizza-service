019e178361fc92aa0cc1e6165636f4e7c9cb74a2
Patrick Blood <pblood66@gmail.com>
2026-01-28 10:25:22 -0700

    test(order): create order

--- src/routes/orderRouter.test.js
+++ src/routes/orderRouter.test.js
@@ -6,6 +6,7 @@ let user = { name: 'diner', password: 'a' };
 let userToken;
 let admin;
 let adminToken;
+let menuItemId;
 
 beforeAll(async () => {
   // normal user
@@ -49,4 +50,29 @@ test('admin adds menu item', async () => {
 
   expect(res.status).toBe(200);
   expect(res.body.some(i => i.title === 'Student Special')).toBe(true);
+  menuItemId = res.body.find(i => i.title === 'Student Special').id;
 });
+
+test('get orders for user', async () => {
+  const res = await request(app)
+    .get('/api/order')
+    .set('Authorization', `Bearer ${userToken}`);
+
+  expect(res.status).toBe(200);
+  expect(res.body.orders).toBeDefined();
+});
+
+test('create order success', async () => {
+  const res = await request(app)
+    .post('/api/order')
+    .set('Authorization', `Bearer ${userToken}`)
+    .send({
+      franchiseId: 1,
+      storeId: 1,
+      items: [{ menuId: menuItemId, description: 'Student Special', price: 0.01 }],
+    });
+
+  expect(res.status).toBe(200);
+  expect(res.body.order).toBeDefined();
+});
+
