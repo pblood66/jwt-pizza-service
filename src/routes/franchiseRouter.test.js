eddf081a865e671354daf1577b6c4377c26b745e
Patrick Blood <pblood66@gmail.com>
2026-01-28 11:04:40 -0700

    test(franchises): list all franchises

--- src/routes/franchiseRouter.test.js
+++ src/routes/franchiseRouter.test.js
@@ -1,39 +1,43 @@
 const request = require('supertest');
 const app = require('../service');
-const { expectValidJwt, createAdminUser } = require('./testUtil');
+const { createAdminUser, expectValidJwt } = require('./testUtil');
+
+let user = { name: 'franchiseUser', password: 'a' };
+let userToken;
+let userId;
 
 let admin;
 let adminToken;
-let user;
-let userToken;
+
+let franchiseId;
+let storeId;
+
+function getIdFromToken(token) {
+  return JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString()).id;
+}
 
 beforeAll(async () => {
-  // Create admin directly
-  admin = await createAdminUser();
+    // create normal user
+    user.email = Math.random().toString(36).slice(2) + '@test.com';
+    const reg = await request(app).post('/api/auth').send(user);
+    userToken = reg.body.token;
+    userId = getIdFromToken(userToken);
+    expectValidJwt(userToken);
 
-  const adminLogin = await request(app)
-    .put('/api/auth')
-    .send({ email: admin.email, password: admin.password });
+    // create admin
+    admin = await createAdminUser();
+    const login = await request(app)
+        .put('/api/auth')
+        .send({ email: admin.email, password: admin.password });
 
-  adminToken = adminLogin.body.token;
-  expectValidJwt(adminToken);
-
-  // Create normal user
-  user = {
-    name: 'normal user',
-    email: Math.random().toString(36).slice(2) + '@test.com',
-    password: 'a',
-  };
-
-  const regRes = await request(app).post('/api/auth').send(user);
-  userToken = regRes.body.token;
-  expectValidJwt(userToken);
+    adminToken = login.body.token;
+    expectValidJwt(adminToken);
 });
 
-test('get all franchises', async () => {
-  const res = await request(app).get('/api/franchise?page=0&limit=10');
+test('List franchises', async () => {
+    const res = await request(app).get('/api/franchise?page=0&limit=10');
 
-  expect(res.status).toBe(200);
-  expect(Array.isArray(res.body.franchises)).toBe(true);
-  expect(res.body.more).toBeDefined();
+    expect(res.status).toBe(200);
+    expect(res.body).toHaveProperty('franchises');
+    expect(res.body).toHaveProperty('more');
 });
