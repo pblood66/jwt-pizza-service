86612cc4476239119d436e177d5ddf75b8d2cc31
Patrick Blood <pblood66@gmail.com>
2026-02-21 16:21:08 -0700

    setup CI workflow to push docker images to ECR

--- src/init.js
+++ src/init.js
@@ -8,5 +8,6 @@ if (process.argv.length < 5) {
 const name = process.argv[2];
 const email = process.argv[3];
 const password = process.argv[4];
-const user = { name, email, password, roles: [{ role: Role.Franchisee }] };
+const user = { name, email, password, roles: [{ role: Role.Admin }] };
+console.log(user)
 DB.addUser(user).then((r) => console.log('created user: ', r));
