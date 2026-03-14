59771f4067e7775067d7f1c06d84b3dd3a78b4a5
Patrick Blood <pblood66@gmail.com>
2026-03-13 21:10:52 -0600

    Add metrics middleware for CPU and memory usage tracking

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const app = require('./service.js');
 
 const port = process.argv[2] || 3000;
+
+const metrics = require('./metrics');
+
+app.use(metrics.requestTracker);
+
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
---
+++ src/metrics.js
@@ -0,0 +1,92 @@
+const config = require('./config');
+const os = require('os');
+
+function getCpuUsagePercentage() {
+  const cpus = os.cpus();
+  let totalIdle = 0, totalTick = 0;
+
+  cpus.forEach((cpu) => {
+    for (const type in cpu.times) {
+      totalTick += cpu.times[type];
+    }
+    totalIdle += cpu.times.idle;
+  });
+
+  return ((1 - totalIdle / totalTick) * 100).toFixed(2);
+}
+
+function getMemoryUsagePercentage() {
+  const totalMemory = os.totalmem();
+  const freeMemory = os.freemem();
+  const usedMemory = totalMemory - freeMemory;
+  const memoryUsage = (usedMemory / totalMemory) * 100;
+  return memoryUsage.toFixed(2);
+}
+
+// Middleware to track requests
+function requestTracker(req, res, next) {
+  next();
+}
+
+// Periodically send CPU and memory metrics to Grafana
+setInterval(() => {
+  const metrics = [
+    createMetric('cpu', getCpuUsagePercentage(), '%', 'gauge', 'asDouble', {}),
+    createMetric('memory', getMemoryUsagePercentage(), '%', 'gauge', 'asDouble', {}),
+  ];
+  sendMetricToGrafana(metrics);
+}, 10000);
+
+function createMetric(metricName, metricValue, metricUnit, metricType, valueType, attributes) {
+  attributes = { ...attributes, source: config.metrics.source };
+  const metric = {
+    name: metricName,
+    unit: metricUnit,
+    [metricType]: {
+      dataPoints: [
+        {
+          [valueType]: metricValue,
+          timeUnixNano: Date.now() * 1000000,
+          attributes: [],
+        },
+      ],
+    },
+  };
+  Object.keys(attributes).forEach((key) => {
+    metric[metricType].dataPoints[0].attributes.push({
+      key: key,
+      value: { stringValue: attributes[key] },
+    });
+  });
+  return metric;
+}
+
+function sendMetricToGrafana(metrics) {
+  const body = {
+    resourceMetrics: [
+      {
+        scopeMetrics: [
+          {
+            metrics,
+          },
+        ],
+      },
+    ],
+  };
+  fetch(`${config.metrics.endpointUrl}`, {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: { Authorization: `Bearer ${config.metrics.accountId}:${config.metrics.apiKey}`, 'Content-Type': 'application/json' },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP status: ${response.status}`);
+      }
+      console.log('Metrics sent successfully');
+    })
+    .catch((error) => {
+      console.error('Error pushing metrics:', error);
+    });
+}
+
+module.exports = { requestTracker };
